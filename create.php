7645bea46d54befca68a778c6988127e32eca52d
herreracadimafabiana <herreracadimafabiana@pedropoveda.edu.bo>
2026-04-08 12:01:42 -0400

    create listojjej

--- create.php
+++ create.php
@@ -7,6 +7,14 @@ $db = "tareitas";
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+   echo "no t conectaste";
 }
+else{
+    echo "si t conectaste" , "<br>";
+}
+$id = $_POST['id'];
+$nombre = $_POST['nombre'];
+$descripcion = $_POST['descripcion'];
+$eestado = $_POST['estado'];
+$sql = "INSERT INTO produc (id, nombre, descripcion, estado) VALUES ('$id', '$nombre', '$descripcion', '$estado')";
 ?>
