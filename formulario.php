26117ea3dd8ce6b12d787472a4de196b7befcdf6
herreracadimafabiana <herreracadimafabiana@pedropoveda.edu.bo>
2026-04-08 11:33:55 -0400

    Create formulario.php
    fformulario fabiaan

---
+++ formulario.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="">
+         <p>nombre</p>
+        <input type="text">
+        <p>descripcion</p>
+        <input type="text">
+         <p>estado</p>
+        <input type="text">
+        <input type="submit" value="enviar">
+        <input type="reset" value="borrar">
+    </form>
+</body>
+</html>
