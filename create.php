4821687909e38d303b8e5a73df71205cb607c26f
Luis Mancilla <mancillaperedoluis@pedropoveda.edu.bo>
2026-04-10 12:05:56 -0400

    Cambios create

--- create.php
+++ create.php
@@ -6,15 +6,19 @@ $db = "tareitas";
 
 $conn = new mysqli($host, $user, $pass, $db);
 
-if ($conn->connect_error) {
+if ($conn->error) {
    echo "no t conectaste";
 }
-else{
-    echo "si t conectaste" , "<br>";
-}
+
 $id = $_POST['id'];
 $nombre = $_POST['nombre'];
 $descripcion = $_POST['descripcion'];
-$eestado = $_POST['estado'];
-$sql = "INSERT INTO produc (id, nombre, descripcion, estado) VALUES ('$id', '$nombre', '$descripcion', '$estado')";
+$tema = $_POST['tema'];
+$sql = "INSERT INTO produc (id, nombre, descripcion, tema) VALUES ('$id', '$nombre', '$descripcion', '$tema')";
+if($conn->query($sql)===TRUE){
+    echo "Se registro correctamente";
+}
+else{
+    echo $sql->error;
+}
 ?>
